1d07f794cac813fcfedb260452926a073d9b81c7
Suji <you@example.com>
2014-09-19 11:59:32 +0530

    adding ring the bell

---
+++ login/application/layouts/main.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+<title>Ozylog{title}</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="http://localhost:8080/login/assets/bootstrap/css/bootstrap.min.css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<style type="text/css">
+	#center {
+    text-align: center;
+}
+
+.ico-information {
+    display: inline-block;
+}
+#footer
+{
+    height: 50px;
+    width: 100%;
+    margin-bottom: 0px;
+}
+.container-full {
+  margin: 0 auto;
+  width: 100%;
+}
+</style>
+</head>
+<body style="color: #413F3F;background-color: #EFF4F5;margin:auto;">
+    <div id="header">
+
+    <!--Navigation Bar -->   
+		 <div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Brand</a>
+  </div>
+  <div class="navbar-collapse collapse navbar-inverse-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Active</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li class="dropdown-header">Dropdown header</li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+    <form class="navbar-form navbar-left">
+      <input type="text" class="form-control col-lg-8" placeholder="Search">
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+	<!-- End of navigation bar -->
+
+	<!-- Header Container -->
+	<div class="container-fluid" >
+	<span class="hidden-xs">
+    <div class="row" id="center" style="background-color:#D0DDD8;padding-top: 75px;padding-bottom: 10px;">
+        <div class="col-xs-4 col-lg-4 col-md-4" style="color:#0C9C74;"><i class="fa fa-search fa-5x fa-border" style="border: solid 1px #0C9C74;"></i><br><h4>Search Your Favorite Story</h4></div>
+        <div class="col-xs-4 col-lg-4 col-md-4" style="color:#0C9C74;"><i class="fa fa-file-text-o fa-5x fa-border" style="border: solid 1px #0C9C74;"></i><br><h4>Read The Full Story With Interesting</h4></div>
+        <div class="col-xs-4 col-lg-4 col-md-4" style="color:#0C9C74;"><i class="fa fa-bell-o fa-5x fa-border" style="border: solid 1px #0C9C74;"></i><br><h4>Ring the Bell (Hand Shake Well)</h4></div>
+    </div>
+    </span>
+    <span class="hidden-md hidden-sm hidden-lg" >
+    <div class="row" id="center" style="background-color:#D0DDD8;padding-top: 70px;padding-bottom: 0px;">
+        <div class="col-xs-4 col-lg-4 col-md-4" style="color:#0C9C74;"><i class="fa fa-search fa-3x fa-border" style="border: solid 1px #0C9C74;"></i><br><h6>Search</h6></div>
+        <div class="col-xs-4 col-lg-4 col-md-4" style="color:#0C9C74;"><i class="fa fa-file-text-o fa-3x fa-border" style="border: solid 1px #0C9C74;"></i><br><h6>Read</h6></div>
+        <div class="col-xs-4 col-lg-4 col-md-4" style="color:#0C9C74;"><i class="fa fa-bell-o fa-3x fa-border" style="border: solid 1px #0C9C74;"></i><br><h6>Hand Shake</h6></div>
+    </div>
+    </span>
+    </div>
+	<!-- End of Header Container -->
+    
+    <div>
+    	{body}
+    </div>
+    
+   <div class="container-fluid" style="background-color:#D0DDD8;">
+       <h1>Footer is Here</h1>
+    </div>
+   
+   <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ login/application/views/home.php
@@ -0,0 +1,159 @@
+<div class="container-fluid" >
+<ol class="breadcrumb">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Library</a></li>
+  <li class="active">Data</li>
+</ol>
+</div>
+<div class="container" style="margin-top:10px;">
+	<div class="row">
+	<span class="hidden-xs">
+		<div class="col-lg-7 col-md-7 col-sm-7">
+		<div style="color:#FFF;background-color:#5E8374;width:100%;padding:5px"> <h4>Latest Stories are Listed Here...</h4></div>
+			<div class="list-group" style="margin-bottom:10px">
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+			</div>
+			<div class="text-center">
+			<ul class="pagination" style="margin: 0px;">
+  <li class="disabled"><a href="#">&laquo;</a></li>
+  <li class="disabled"><a href="#">1</a></li>
+ <li class="active"><a href="#">2 <span class="sr-only">(current)</span></a></li>
+ <li class="disabled"><a href="#">3 </a></li>
+ <li class="disabled"><a href="#">4 </a></li>
+ <li class="disabled"><a href="#">5 </a></li>
+  <li class="disabled"><a href="#">&laquo;</a></li>
+</ul>
+</div>
+		</div>
+		<div class="col-lg-5 col-md-5 col-sm-5">
+			
+
+<div class="well">
+<form class="form-horizontal">
+  <fieldset>
+    <legend>plz Signup for Write your own story</legend>
+    <div class="form-group">
+      <label for="inputEmail" class="col-lg-5 control-label">Name</label>
+      <div class="col-lg-7">
+        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="inputEmail" class="col-lg-5 control-label">User Name</label>
+      <div class="col-lg-7">
+        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="inputPassword" class="col-lg-5 control-label">Password</label>
+      <div class="col-lg-7">
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+      </div>
+    </div>
+     <div class="form-group">
+      <label for="inputPassword" class="col-lg-5 control-label">Retype Password</label>
+      <div class="col-lg-7">
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-lg-10 col-lg-offset-2">
+        <button class="btn btn-default">Cancel</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </fieldset>
+</form>
+
+</div>
+
+		</div>
+	</span>
+	<span class="hidden-md hidden-sm hidden-lg" >
+		<div class="col-xs-12">
+			<div style="color:#FFF;background-color:#5E8374;width:100%;padding:5px"> <h4>Latest Stories are Listed Here...</h4></div>
+			<div class="list-group">
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+				  <a href="#" class="list-group-item">
+				    <h4 class="list-group-item-heading">List group item heading</h4>
+				    <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+				  </a>
+			</div>
+		</div>
+	</span>
+	</div>
+</div>
