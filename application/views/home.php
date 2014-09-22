08a85cfd3a53a168e05e6315eabdc91c2663f1c4
seegan <see.gan21@gmail.com>
2014-09-22 12:34:21 +0530

    new view

--- application/views/home.php
+++ application/views/home.php
@@ -70,44 +70,7 @@
 			<div class="col-lg-5 col-md-5 col-sm-5">
 				
 
-				<div class="well">
-					<form class="form-horizontal">
-					  <fieldset>
-					    <legend>plz Signup for Write your own story</legend>
-					    <div class="form-group">
-					      <label for="inputEmail" class="col-lg-5 control-label">Name</label>
-					      <div class="col-lg-7">
-					        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
-					      </div>
-					    </div>
-					    <div class="form-group">
-					      <label for="inputEmail" class="col-lg-5 control-label">User Name</label>
-					      <div class="col-lg-7">
-					        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
-					      </div>
-					    </div>
-					    <div class="form-group">
-					      <label for="inputPassword" class="col-lg-5 control-label">Password</label>
-					      <div class="col-lg-7">
-					        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-					      </div>
-					    </div>
-					     <div class="form-group">
-					      <label for="inputPassword" class="col-lg-5 control-label">Retype Password</label>
-					      <div class="col-lg-7">
-					        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-					      </div>
-					    </div>
-					    <div class="form-group">
-					      <div class="col-lg-10 col-lg-offset-2">
-					        <button class="btn btn-default">Cancel</button>
-					        <button type="submit" class="btn btn-primary">Submit</button>
-					      </div>
-					    </div>
-					  </fieldset>
-					</form>
-
-				</div>
+				<?php $this->load->view('sidebar/register'); ?>
 
 			</div>
 		</span>
