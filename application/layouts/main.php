2422d7d6589c25f52cf190e098eac29ff8568c33
Suji <you@example.com>
2014-09-22 13:08:35 +0530

    addesd

--- application/layouts/main.php
+++ application/layouts/main.php
@@ -126,7 +126,7 @@
   </div>
   <!-- End of Breadcrumb -->
   <!-- Info message -->
-  <div class="alert alert-success" role="alert">...</div>
+  <div class="alert alert-success" role="alert">hii mokka seega</div>
   <div class="alert alert-info" role="alert">...</div>
   <div class="alert alert-warning" role="alert">...</div>
   <div class="alert alert-danger" role="alert">...</div>
