b3e290514dba8b532cca1c331bce9c0b75c81081
Suji <you@example.com>
2014-09-23 16:11:45 +0530

    hbjhvjb

--- application/views/posts/posts.php
+++ application/views/posts/posts.php
@@ -2,30 +2,25 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8">
 		<div class="list-group" style="margin-bottom:10px">
-			<a class="list-group-item">
+			<a class="list-group-item" style="padding:2px;border-left:solid 2px #23B6B0">
 			<div class="row" >
-				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2" style="background-color: #FAF8F4;">
-					<div style="margin:0 auto;width:90%">
-						<div class="row" style="margin:5px;">
-							
-							<i class="fa fa-thumbs-up"> 123</i>
-							
-						</div>
-						<div class="row" style="margin:5px;">
-						
-							<i class="fa fa-wechat"> 45</i>
-								
-						</div>
-						<div class="row" style="margin:5px;">
-							
-							<i class="fa fa-thumbs-down"> 67</i>
-								
-						</div>
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center" style="padding-right:0px;font-size:14px;padding-top: 10px;" >
+					<div class="row" style="margin:5px; color:#509622;">	
+						<i class="fa fa-thumbs-up"> 123</i>
+					</div>
+					<div class="row" style="margin:5px;color:#51A5BA;">
+						<i class="fa fa-wechat"> 45</i>		
+					</div>
+					<div class="row" style="margin:5px;color:#E04D1E;">		
+						<i class="fa fa-thumbs-down"> 67</i>		
 					</div>
 				</div>
-				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" style="padding-left:0px">
+				<div style="padding:10px">
 					<h4 class="list-group-item-heading">List group item heading</h4>
 					<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+					<p style="margin-top:5px;color:#51A5BA;">Posted by Admin on 24-9-12 10.30PM</p>
+				</div>	
 				</div>
 			</div>
 			</a>
