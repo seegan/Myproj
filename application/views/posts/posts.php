1f0d1d5cfcca1db58257663a9000c7f270239791
Suji <you@example.com>
2014-09-25 13:16:55 +0530

    added

--- application/views/posts/posts.php
+++ application/views/posts/posts.php
@@ -1,7 +1,73 @@
-<div class="container">
+<div class="container" style="margin-top:40px">
 <div class="row">
 	<div class="col-lg-8 col-md-8">
 		<div class="list-group" style="margin-bottom:10px">
+			<a class="list-group-item" style="padding:2px;border-left:solid 2px #23B6B0">
+			<div class="row" >
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center" style="padding-right:0px;font-size:14px;padding-top: 10px;" >
+					<div class="row" style="margin:5px; color:#509622;">	
+						<i class="fa fa-thumbs-up"> 123</i>
+					</div>
+					<div class="row" style="margin:5px;color:#51A5BA;">
+						<i class="fa fa-wechat"> 45</i>		
+					</div>
+					<div class="row" style="margin:5px;color:#E04D1E;">		
+						<i class="fa fa-thumbs-down"> 67</i>		
+					</div>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" style="padding-left:0px">
+				<div style="padding:10px">
+					<h4 class="list-group-item-heading">List group item heading</h4>
+					<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+					<p style="margin-top:5px;color:#51A5BA;">Posted by Admin on 24-9-12 10.30PM</p>
+				</div>	
+				</div>
+			</div>
+			</a>
+			<a class="list-group-item" style="padding:2px;border-left:solid 2px #23B6B0">
+			<div class="row" >
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center" style="padding-right:0px;font-size:14px;padding-top: 10px;" >
+					<div class="row" style="margin:5px; color:#509622;">	
+						<i class="fa fa-thumbs-up"> 123</i>
+					</div>
+					<div class="row" style="margin:5px;color:#51A5BA;">
+						<i class="fa fa-wechat"> 45</i>		
+					</div>
+					<div class="row" style="margin:5px;color:#E04D1E;">		
+						<i class="fa fa-thumbs-down"> 67</i>		
+					</div>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" style="padding-left:0px">
+				<div style="padding:10px">
+					<h4 class="list-group-item-heading">List group item heading</h4>
+					<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+					<p style="margin-top:5px;color:#51A5BA;">Posted by Admin on 24-9-12 10.30PM</p>
+				</div>	
+				</div>
+			</div>
+			</a>
+			<a class="list-group-item" style="padding:2px;border-left:solid 2px #23B6B0">
+			<div class="row" >
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center" style="padding-right:0px;font-size:14px;padding-top: 10px;" >
+					<div class="row" style="margin:5px; color:#509622;">	
+						<i class="fa fa-thumbs-up"> 123</i>
+					</div>
+					<div class="row" style="margin:5px;color:#51A5BA;">
+						<i class="fa fa-wechat"> 45</i>		
+					</div>
+					<div class="row" style="margin:5px;color:#E04D1E;">		
+						<i class="fa fa-thumbs-down"> 67</i>		
+					</div>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" style="padding-left:0px">
+				<div style="padding:10px">
+					<h4 class="list-group-item-heading">List group item heading</h4>
+					<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+					<p style="margin-top:5px;color:#51A5BA;">Posted by Admin on 24-9-12 10.30PM</p>
+				</div>	
+				</div>
+			</div>
+			</a>
 			<a class="list-group-item" style="padding:2px;border-left:solid 2px #23B6B0">
 			<div class="row" >
 				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center" style="padding-right:0px;font-size:14px;padding-top: 10px;" >
@@ -28,6 +94,40 @@
 	</div>
 	<div class="col-lg-4 col-md-4">
 		
+		<div class="panel panel-info">
+			<div class="panel-heading"><i class="fa fa-female fa-2x"> </i> Related posts</div>
+
+			<div style="margin:0 auto;width:90%;">
+					<ul style="background-color:#fff;list-style-type: none;margin: 0;padding: 10px;">
+					<li style="border-bottom: dashed 1px #B0E6DD;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #B0E6DD;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #B0E6DD;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #B0E6DD;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #B0E6DD;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #B0E6DD;padding-bottom: 2px;">First post</li>
+					</ul>
+			</div>
+
+		</div>
+
+		
+			<div class="panel-heading"><i class="fa fa-bomb fa-2x"> </i> Hot posts</div>
+				
+				<div style="margin:0 auto;width:90%;">
+					<ul style="background-color:#fff;list-style-type: none;margin: 0;padding: 10px;">
+					<li style="border-bottom: dashed 1px #E6B0B0;padding-bottom: 2px;">First post
+					<a class="btn btn-info btn-xs">Read More</a>
+					</li>
+					<li style="border-bottom: dashed 1px #E6B0B0;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #E6B0B0;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #E6B0B0;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #E6B0B0;padding-bottom: 2px;">First post</li>
+					<li style="border-bottom: dashed 1px #E6B0B0;padding-bottom: 2px;">First post</li>
+					</ul>
+				</div>
+		
+		</div>
+
 	</div>
 
 </div>
